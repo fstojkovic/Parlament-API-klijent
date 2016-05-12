9d2facdefd07bf15c4147cd403d020eab2b8c6a8
fstojkovic <filip.cofi.stojkovic@gmail.com>
2016-05-12 19:22:52 +0200

    Kreirana klasa poslanik i klasa ParlamentAPIKomunikacija zadužena za
    komunikaciju sa servisom OpenParliament Rest API.

---
+++ clanoviParlamenta/src/domain/Poslanik.java
@@ -0,0 +1,66 @@
+package domain;
+
+import java.util.Date;
+
+public class Poslanik {
+
+	private int id;
+	private String ime;
+	private String prezime;
+	private Date datumRodjenja;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getIme() {
+		return ime;
+	}
+
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+
+	public String getPrezime() {
+		return prezime;
+	}
+
+	public void setPrezime(String prezime) {
+		this.prezime = prezime;
+	}
+
+	public Date getDatumRodjenja() {
+		return datumRodjenja;
+	}
+
+	public void setDatumRodjenja(Date datumRodjenja) {
+		this.datumRodjenja = datumRodjenja;
+	}
+
+	@Override
+	public String toString() {
+		return "Member [id=" + id + ", ime=" + ime + ", prezime=" + prezime + ", datumRodjenja=" + datumRodjenja + "]";
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Poslanik other = (Poslanik) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
